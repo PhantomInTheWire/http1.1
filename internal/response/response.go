b47502bc8d17beaac5af31dc37ac117398d28e61
Karan <karanlokchandani@protonmail.com>
2025-08-26 13:40:09 +0530

    feat: chunked encoding

--- internal/response/response.go
+++ internal/response/response.go
@@ -115,3 +115,36 @@ func (w *Writer) WriteBody(p []byte) (int, error) {
 	w.state = StateBodyWritten
 	return n, nil
 }
+
+func (w *Writer) WriteChunk(p []byte) (int, error) {
+	if len(p) == 0 {
+		return 0, nil
+	}
+
+	_, err := fmt.Fprintf(w.w, "%x\r\n", len(p))
+	if err != nil {
+		return 0, err
+	}
+
+	n, err := w.w.Write(p)
+	if err != nil {
+		return n, err
+	}
+
+	_, err = w.w.Write([]byte("\r\n"))
+	if err != nil {
+		return n, err
+	}
+
+	return n, nil
+}
+
+func (w *Writer) WriteChunkedBodyDone() error {
+	// Write the final zero-length chunk
+	_, err := w.w.Write([]byte("0\r\n\r\n"))
+	return err
+}
+
+func (w *Writer) WriteTrailers(h headers.Headers) error {
+	w.w.Write(header)
+}
